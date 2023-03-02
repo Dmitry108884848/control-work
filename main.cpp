0451ea5f9f82c38718048a862ea1d8fa6a4fcb3e
Dmitry108884848 <112952049+Dmitry108884848@users.noreply.github.com>
2023-03-02 11:47:07 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+int main (){
+int num;
+    int b;
+   std:: cout<<"vedite number";
+   std:: cin<<num;
+    for(int i= -4; i>=num;i-=6){
+        b+=i;   
+     }
+    std:: cout<<b;
+}
